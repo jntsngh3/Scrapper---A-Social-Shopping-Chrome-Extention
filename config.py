ec7c7d6bb6bef060aeca3dd746649d18f93c3858
Jayant Singh <jntsngh3@gmail.com>
2018-09-01 00:38:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = 'BzLKpZ8xC0OcKkmtIuOzS0sKM'
-consumer_secret = 'MULxzM44UgZIVYTmYfKsiwYQhE0JG4p6mDA5RJhHRWyMqKHNhA'
-access_token = '2830219376-Sjk8YvZtWSdxZxdZyy5t91wtwIHd2FYpAadWnmJ'
-access_token_secret = 'RAhkc6SY72fkolUfAjCnFO5yfKcOkqlxtjluh0Ece9Xqj'
+consumer_key = 'BzLKpZ8xC0Oc****IuOzS0sKM'
+consumer_secret = 'MULxzM44UgZIV***KsiwYQhE0JG4p6mDA5RJhHRWyMqKHNhA'
+access_token = '2830219376-Sjk8YvZtW8***xdZyy5t91wtwIHd2FYpAadWnmJ'
+access_token_secret = 'RAhkc6SY72fkolU****nFO5yfKcOkqlxtjluh0Ece9Xqj'
